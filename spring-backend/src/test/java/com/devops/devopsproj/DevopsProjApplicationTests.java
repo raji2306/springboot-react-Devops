49d6d17dca5048e10a6128a88c8ed85c5cf3c271
Shubham Singh <53940547+shubhamsingh93@users.noreply.github.com>
2023-12-19 15:47:13 +0530

    Updated comment

--- spring-backend/src/test/java/com/devops/devopsproj/DevopsProjApplicationTests.java
+++ spring-backend/src/test/java/com/devops/devopsproj/DevopsProjApplicationTests.java
@@ -3,6 +3,8 @@ package com.devops.devopsproj;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//this is a comment
+
 @SpringBootTest
 class DevopsProjApplicationTests {
 
